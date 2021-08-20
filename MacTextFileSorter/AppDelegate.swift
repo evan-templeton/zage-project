653d706137482e1cc20e8c1c98a59d6297f64aeb
Evan Templeton <temevan@amazon.com>
2021-08-20 16:22:13 -0500

    Initial Commit

---
+++ MacTextFileSorter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacTextFileSorter
+//
+//  Created by Templeton, Evan on 8/20/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
